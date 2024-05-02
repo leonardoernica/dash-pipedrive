9904542b4e587b2b889ef6146286f9aa30bb5439
Leonardo Ernica <135552943+leonardoernica@users.noreply.github.com>
2024-05-02 17:32:43 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -2,16 +2,9 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from datetime import datetime, timedelta
-import schedule
-import time
-from app import *
-schedule.every(1).hour.do(update_deals_csv)  # run every 1 hour
+from apscheduler.schedulers.background import BackgroundScheduler
+from app import update_deals_csv
 
-while True:
-    schedule.run_pending()
-    time.sleep(60)  # sleep for 1 minute
-
-@st.cache_data
 def load_data():
     try:
         data = pd.read_csv('deals.csv', parse_dates=['Start Date', 'End Date'])
@@ -22,9 +15,21 @@ def load_data():
         return pd.DataFrame()
 
 st.title('Dashboard Comercial - Soluções Hyper')
-df = load_data()
 
-if not df.empty:
+# Inicializa o agendador
+scheduler = BackgroundScheduler()
+scheduler.add_job(update_deals_csv, 'interval', hours=1)
+scheduler.start()
+
+@st.cache_data(ttl=4000, show_spinner=True)
+def get_cached_data():
+    return load_data()
+
+df = get_cached_data()
+
+if df.empty:
+    st.write("Nenhum dado disponível para exibir.")
+else:
     default_start_date = datetime.now() - timedelta(days=7)
     default_end_date = datetime.now()
     start_date = st.sidebar.date_input("Data Início", default_start_date)
@@ -32,39 +37,38 @@ if not df.empty:
     owner_list = ['Todos'] + sorted(df['Owner Name'].unique())
     selected_owners = st.sidebar.multiselect('Selecione o Dono do Negócio', owner_list, default='Todos')
 
-    # Filtragem usando End Date e Status de 'won'
     filtered_df = df[(df['Status'] == 'won') & 
                      (df['End Date'].dt.date >= start_date) & 
                      (df['End Date'].dt.date <= end_date) &
                      ((df['Owner Name'].isin(selected_owners)) if 'Todos' not in selected_owners else True)]
 
-    # Garantindo que consideramos apenas o End Date mais recente de cada Deal ID
-    filtered_df = filtered_df.sort_values(by='End Date').drop_duplicates(subset=['Deal ID'], keep='last')
+    if filtered_df.empty:
+        st.write("Sem resultados para esta pesquisa.")
+    else:
+        faturamento = filtered_df.groupby(filtered_df['End Date'].dt.date)['Value'].sum()
+        fig_faturamento = px.line(
+            x=faturamento.index, y=faturamento.values,
+            labels={'x': 'Data', 'y': 'Faturamento'}, title='Faturamento'
+        )
+        fig_faturamento.update_xaxes(tickformat="%d/%m/%Y")
+        fig_faturamento.update_layout(xaxis_title="Data", yaxis_title="Faturamento (R$)")
+        st.write(f"Faturamento Total: R$ {faturamento.sum():,.2f}")
+        st.plotly_chart(fig_faturamento, use_container_width=True)
 
-    # Agrupando pelo End Date e somando os valores
-    faturamento = filtered_df.groupby(filtered_df['End Date'].dt.date)['Value'].sum()
-
-    fig_faturamento = px.line(
-        x=faturamento.index, y=faturamento.values,
-        labels={'x': 'Data', 'y': 'Faturamento'}, title='Faturamento'
-    )
-    fig_faturamento.update_xaxes(tickformat="%d/%m/%Y")
-    fig_faturamento.update_layout(xaxis_title="Data", yaxis_title="Faturamento (R$)")
-    st.write(f"Faturamento Total: R$ {faturamento.sum():,.2f}")
-    st.plotly_chart(fig_faturamento, use_container_width=True)
-
-    # Filtragem usando Start Date
     funnel_df = df[(df['pipeline_id']==1) &
                    (df['Start Date'].dt.date >= start_date) & 
                    (df['Start Date'].dt.date <= end_date) &
                    ((df['Owner Name'].isin(selected_owners)) if 'Todos' not in selected_owners else True)]
 
-    # Reordenando os dados
-    stage_counts = funnel_df['Stage ID'].value_counts().reset_index()
-    stage_counts.columns = ['Stage ID', 'Count']
-    stage_counts = stage_counts.sort_values(by='Stage ID')  # Supondo que Stage ID menor é o topo do funil
-
-    # Criando o gráfico de funil
-    fig_funnel = px.funnel(stage_counts, x='Count', y='Stage ID', orientation='h', title='Funil de Vendas por Stage ID')
-    fig_funnel.update_layout(yaxis={'categoryorder':'total ascending'})
-    st.plotly_chart(fig_funnel, use_container_width=True)
+    if funnel_df.empty:
+        st.write("Sem dados no funil para esta pesquisa.")
+    else:
+        # Adicionando garantia de representação de todos os Stages, mesmo que seja zero
+        all_stages = pd.DataFrame({'Stage ID': range(1, 8), 'Count': [0]*7})  # Adapte conforme o número de Stages
+        stage_counts = funnel_df['Stage ID'].value_counts().reset_index()
+        stage_counts.columns = ['Stage ID', 'Count']
+        stage_counts = pd.merge(all_stages, stage_counts, on='Stage ID', how='left').fillna(0)
+        stage_counts['Count'] = stage_counts['Count_y']
+        fig_funnel = px.funnel(stage_counts, x='Count', y='Stage ID', orientation='h', title='Funil de Vendas por Stage ID')
+        fig_funnel.update_layout(yaxis={'categoryorder':'total ascending'})
+        st.plotly_chart(fig_funnel, use_container_width=True)
